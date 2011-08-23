0deed6960ee2c574dac57e0d42f8a8da9b509442
Glen Maddern <glenmaddern@gmail.com>
2011-08-23 14:51:59 +1000

    added the user

---
+++ User.h
@@ -0,0 +1,16 @@
+//
+//  Queue.h
+//  goodfilms
+//
+//  Created by Glen Maddern on 23/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+// Contains Films
+@property (readonly) NSArray *queue;
+
+@end
---
+++ User.m
@@ -0,0 +1,23 @@
+//
+//  Queue.m
+//  goodfilms
+//
+//  Created by Glen Maddern on 23/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
